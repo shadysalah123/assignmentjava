a1bfdf6f52b2e3d4bfe4a6b9a93f0eec4f1e2b32
Shady Salah <108495897+shadysalah123@users.noreply.github.com>
2025-07-10 16:33:24 +0300

    Add files via upload

---
+++ assignment1.java
@@ -0,0 +1,14 @@
+public class assignment1 {
+    public static void main (String[] args) {
+        //variablles//
+        String name = "Mo Salah" , FPL="Java" ;
+        int age = 25;
+
+        System.out.println("Hello! My name is " + name );
+        System.out.println("I am " + age + " years old, which is " + age*12 + " months");
+        System.out.println("My favorite programming language is " + FPL );
+        age++ ;
+        System.out.println("Next year I will be " + age + " years old" );
+    }
+}
+
